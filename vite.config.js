7226b6748ac141d86681daacbad182ab3ddc349b
yuhoulaiqiu <3240858086@qq.com>
2026-03-16 16:06:43 +0800

    服务器配置

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      host: '114.117.200.17',
+    },
+    proxy: {
+      '/api': {
+        target: 'http://114.117.200.17:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
